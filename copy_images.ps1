6c71f1b35b14c3a9fb062433fc8aa3d874f99050
Murat <murat@ryadom-svami.ru>
2026-04-16 22:36:32 +0300

    Remove sensitive files from repo

--- copy_images.ps1
+++
@@ -1,8 +0,0 @@
-cp "C:\Users\Мурат\.gemini\antigravity\brain\71308ab2-405c-4e05-9dbc-daa77a6d0275\test_3d_icon_autism_1776337676004.png" "C:\Users\Мурат\Desktop\сайт\img\icon_autism.png"
-cp "C:\Users\Мурат\.gemini\antigravity\brain\71308ab2-405c-4e05-9dbc-daa77a6d0275\icon_cp_b_1776338034645.png" "C:\Users\Мурат\Desktop\сайт\img\icon_cp.png"
-cp "C:\Users\Мурат\.gemini\antigravity\brain\71308ab2-405c-4e05-9dbc-daa77a6d0275\icon_down_b_1776337949895.png" "C:\Users\Мурат\Desktop\сайт\img\icon_down.png"
-cp "C:\Users\Мурат\.gemini\antigravity\brain\71308ab2-405c-4e05-9dbc-daa77a6d0275\icon_speech_a_1776337964056.png" "C:\Users\Мурат\Desktop\сайт\img\icon_speech.png"
-cp "C:\Users\Мурат\.gemini\antigravity\brain\71308ab2-405c-4e05-9dbc-daa77a6d0275\icon_uo_a_1776338070670.png" "C:\Users\Мурат\Desktop\сайт\img\icon_uo.png"
-cp "C:\Users\Мурат\.gemini\antigravity\brain\71308ab2-405c-4e05-9dbc-daa77a6d0275\icon_okr_a_1776338097312.png" "C:\Users\Мурат\Desktop\сайт\img\icon_okr.png"
-cp "C:\Users\Мурат\.gemini\antigravity\brain\71308ab2-405c-4e05-9dbc-daa77a6d0275\icon_test_a_1776342802001.png" "C:\Users\Мурат\Desktop\сайт\img\icon_test.png"
-Write-Host "Все иконки скопированы!" -ForegroundColor Green
